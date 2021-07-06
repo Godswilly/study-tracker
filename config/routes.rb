1c841cbca85917a82055f60209ca83c4e13d8260
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-07-06 11:27:21 +0100

    Implement necessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'levels/progress'
+  resources :studies, except: [:index]
+
+  get '/allData', to: 'studies#index'
+
+  post '/signup', to: 'users#create'
+  post 'auth/login', to: 'authentication#authenticate'
 end
