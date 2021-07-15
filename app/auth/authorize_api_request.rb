8636429dbd4f61557093d48bbd25f7c37df61d3f
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-07-15 16:29:52 +0100

    Fix linter errors

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -23,7 +23,7 @@ class AuthorizeApiRequest
   end
 
   def http_auth_header
-    return headers['Authorization'].split(' ').last if headers['Authorization'].present?
+    return headers['Authorization'].split.last if headers['Authorization'].present?
 
     raise(ExceptionHandler::MissingToken, Message.missing_token)
   end
