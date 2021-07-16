d607ac9a04fd4ad7d165072cba76a4abbc8129c5
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-07-16 11:44:27 +0100

    Update progress calculation method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,4 @@ class ApplicationController < ActionController::API
     @current_user = AuthorizeApiRequest.call(request.headers).result
     render json: { error: 'Not Authorized' }, status: 401 unless @current_user
   end
-
-  def progress_calculation(study)
-    hash = {}
-    hash['hours'] = study.inject(0) { |sum, e| sum + e.hours }
-    hash['goal'] = study.inject(0) { |sum, e| sum + e.goal }
-    hash
-  end
 end
--- app/helpers/study_helper.rb
+++
@@ -1,8 +0,0 @@
-module StudyHelper
-  def self.progress_calculation(study)
-    hash = {}
-    hash['hours'] = study.inject(0) { |sum, e| sum + e.hours }
-    hash['goal'] = study.inject(0) { |sum, e| sum + e.goal }
-    hash
-  end
-end
